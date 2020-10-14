b8aea0359d43c4e17fa46824d0d359f16fd30f74
Cristóbal Martínez <wipeoman@gmail.com>
2020-10-14 09:21:52 +0200

    Initial commit

---
+++ src/control/Main.java
@@ -0,0 +1,12 @@
+package control;
+
+public class Main {
+	private int[] board = new int[] {4,3};
+	private int[][] snake;
+	private int depth;
+
+	public static void main(String[] args) {
+
+	}
+
+}
